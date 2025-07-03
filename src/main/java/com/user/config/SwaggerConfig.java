6264c0c3475ab07b27b2778f3cceb778a16861a3
Munyaradzi Linos Mudukuti <munyaradzilinosmudukuti@gmail.com>
2025-07-03 10:43:47 +0200

    refactor: pom.xml, SwaggerConfig - Update dependencies
    feat: pom.xml - Add necessary dependencies
    
    chore: SystemApplicationTests - Remove test class

--- src/main/java/com/user/config/SwaggerConfig.java
+++ src/main/java/com/user/config/SwaggerConfig.java
@@ -1,23 +1,18 @@
 package com.user.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "User Manager Turtorial")
-)
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public GroupedOpenApi userTutorial() {
-//        return GroupedOpenApi.builder().
-//                group("User Tutorial")
-//                .packagesToScan("com.user")
-//                .build();
-//    }
-
+    @Bean
+    public GroupedOpenApi roadWatchSwagger() {
+        return GroupedOpenApi.builder()
+                .group("Tanaka User Management System APIs")
+                .packagesToScan("com.user")
+                .build();
+    }
 }
+
